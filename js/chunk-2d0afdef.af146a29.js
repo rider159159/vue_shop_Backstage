bb02a9636c9223e6dc6d0689b3d8feb1be90855e
Ryder <rider159159@gmail.com>
2021-02-20 20:27:24 +0800

    deploy

---
+++ js/chunk-2d0afdef.af146a29.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0afdef"],{"0fbe":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("p",[e._v("最新消息")])])}],u={},l=u,a=t("2877"),s=Object(a["a"])(l,c,r,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0afdef.af146a29.js.map
